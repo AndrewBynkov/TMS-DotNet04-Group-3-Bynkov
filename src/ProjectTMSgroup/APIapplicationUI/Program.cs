68d0af64a1af49f3a5ccf3c48432ecfa2280525b
Andrew <andrew_bynkov@mail.ru>
2020-11-18 21:39:46 +0300

    feat: create project delete old version

---
+++ src/ProjectTMSgroup/APIapplicationUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APIapplicationUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
