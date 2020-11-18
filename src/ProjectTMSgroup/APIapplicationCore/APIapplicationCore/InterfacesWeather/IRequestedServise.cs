f3afa4af298dee2c27ce45af7149b244297b9b00
Andrew <andrew_bynkov@mail.ru>
2020-11-18 21:46:23 +0300

    feat: create interface IRequestedServise, create class RequestedServise, add namespases

---
+++ src/ProjectTMSgroup/APIapplicationCore/APIapplicationCore/InterfacesWeather/IRequestedServise.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIapplicationCore.InterfacesWeather
+{
+    public interface IRequestedServise
+    {
+        public void Request();
+    }
+}
