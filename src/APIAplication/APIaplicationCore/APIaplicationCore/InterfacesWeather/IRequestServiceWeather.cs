6c91bf5865417568b4b668aadc34be7247138a3c
Andrew <andrew_bynkov@mail.ru>
2020-11-18 20:33:27 +0300

    feat: delete project and create new project

---
+++ src/APIAplication/APIaplicationCore/APIaplicationCore/InterfacesWeather/IRequestServiceWeather.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIapplicationCore.InterfacesWeather
+{
+    public interface IRequestServiceWeather
+    {
+        public void Request();
+    }
+}
---
+++ src/APIAplication/APIaplicationCore/APIaplicationCore/ServicesWeather/RequestServiceWeather.cs
@@ -0,0 +1,15 @@
+﻿using APIapplicationCore.InterfacesWeather;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIapplicationCore.ServicesWeather
+{
+    class RequestServiceWeather : IRequestServiceWeather
+    {
+        public void Request()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
