82116ef0c5f7e963e5ecdef72f60dd6d9bf982c1
Andrew <andrew_bynkov@mail.ru>
2020-11-18 20:52:45 +0300

    feat: new proj

---
+++ src/APIapplicationCore/APIapplicationCore/InterfacesWeather/IRequestServices.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIapplicationCore.InterfacesWeather
+{
+    public interface IRequestServices
+    {
+        public void Request();
+    }
+}
